f7bd79dff8986a4224010e6d92aadf7de8252249
Daniel <daniel.alp.email@gmail.com>
2023-08-18 19:11:11 -0400

    Implemented move representation.

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,26 @@
 #include "bitboard.h"
 #include "board.h"
+#include "move.h"
 #include "types.h"
 #include <iostream>
 
-//TODO add debug statements
 int main(int argc) {
 	precompute_rays();
 
-	Position pos{};
+	uint32_t move = build_move(20, 40, Piece::WHITE_KNIGHT, Piece::BLACK_QUEEN, flag_en_passant);
+	std::cout << move_from_sq(move) << std::endl;
+	std::cout << move_to_sq(move) << std::endl;
+	std::cout << static_cast<uint32_t>(move_capture_pce(move)) << std::endl;
+	std::cout << static_cast<uint32_t>(move_promo_pce(move)) << std::endl;
+	std::cout << (move & flag_en_passant) << std::endl;
+	std::cout << (move & flag_pawn_start) << std::endl;
+	std::cout << (move & flag_castle) << std::endl;
+
+
+	/*Position pos{};
 	const std::string start_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 	load_from_fen(pos, start_fen);
-	print_board(pos);
-	print_bitboard(pos.all_bitboard);
-	print_bitboard(pos.col_bitboards[0]);
-	print_bitboard(pos.col_bitboards[1]);
+	print_board(pos);*/
 
 	return 0;
 }
---
+++ src/move.cpp
@@ -0,0 +1,33 @@
+#include "board.h"
+#include "move.h"
+#include "types.h"
+#include <cstdint>
+#include <string>
+
+uint32_t flag_none		 = 0b00000000000000000000000;
+uint32_t flag_pawn_start = 0b00100000000000000000000;
+uint32_t flag_castle	 = 0b01000000000000000000000;
+uint32_t flag_en_passant = 0b10000000000000000000000;
+
+std::string get_move_str(uint32_t move) {
+	uint32_t from_sq = move_from_sq(move);
+	int from_rank = get_rank(from_sq);
+	int from_file = get_file(from_sq);
+	uint32_t to_sq = move_to_sq(move);
+	int to_rank = get_rank(to_sq);
+	int to_file = get_file(to_sq);
+
+	std::string move_str;
+	move_str.push_back('a' + from_file);
+	move_str.push_back(from_rank + 1);
+	move_str.push_back('a' + to_file);
+	move_str.push_back(to_rank + 1);
+
+	Piece promo_pce = move_promo_pce(move);
+
+	if (static_cast<uint32_t>(promo_pce)) {
+		move_str.push_back(pce_to_symbol(promo_pce));
+	}
+
+	return move_str;
+}

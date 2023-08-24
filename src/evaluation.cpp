b0f3774c4a5848ea0e0c15dc602fe5f68fdfed3c
Daniel Alp <77411550+Daniel-Alp@users.noreply.github.com>
2023-08-24 01:06:34 -0400

    Implemented negamax with Donald Knuth alpha beta pruning. (#1)

---
+++ src/evaluation.cpp
@@ -0,0 +1,25 @@
+#include "board.h"
+#include "bitboard.h"
+#include "evaluation.h"
+#include "types.h"
+#include <cstdint>
+
+#include <iostream>
+
+int32_t evaluate(Position& pos) {
+	int32_t midgame_eval = 0;
+	for (int col = static_cast<int>(Color::WHITE); col <= static_cast<int>(Color::BLACK); col++) {
+		for (int pce_type = static_cast<int>(PieceType::PAWN); pce_type <= static_cast<int>(PieceType::KING); pce_type++) {
+			uint64_t pce_bitboard = pos.pce_bitboards[build_pce(pce_type, col)];
+			while (pce_bitboard != 0) {
+				midgame_eval += midgame_pce_vals[pce_type];
+				pce_bitboard = clr_lsb(pce_bitboard);
+			}
+		}
+		midgame_eval *= -1;
+	}
+	if (pos.side_to_move == Color::BLACK) {
+		midgame_eval *= -1;
+	}
+	return midgame_eval;
+}
--- src/search.cpp
+++ src/search.cpp
@@ -1,10 +1,11 @@
+#include "attacks.h"
+#include "evaluation.h"
 #include "search.h"
 #include "move.h"
 #include "movegen.h"
 #include "makemove.h"
-
+#include <cstdint>
 #include "cstdlib"
-
 #include <iostream>
 
 bool searching;
@@ -12,25 +13,50 @@ bool searching;
 void get_best_move(Position& pos) {
 	searching = true;
 	pos.ply = 0;
-	uint32_t best_move = null_move;
+	uint32_t best_move_root = null_move;
+	negamax(pos, best_move_root, -mate_score, mate_score, 4, 0);
+	searching = false;
+	std::cout << "bestmove " << get_move_str(best_move_root) << std::endl;
+}
 
-	//Returns random legal move in position
-	std::array<uint32_t, max_moves> pseudo_moves;
-	int num_pseudo_moves;
-	gen_pseudo_moves(pos, pseudo_moves, num_pseudo_moves, false);
+//Alpha-beta implementation by Donald Knuth
+int32_t negamax(Position& pos, uint32_t& best_move_root, int32_t alpha, int32_t beta, int depth, int ply) {
+	int32_t best_score = alpha;
 
-	std::array<uint32_t, max_moves> legal_moves;
-	int num_legal_moves = 0;
-
-	for (int i = 0; i < num_pseudo_moves; i++) {
-		if (make_move(pos, pseudo_moves[i])) {
-			undo_move(pos, pseudo_moves[i]);
-			legal_moves[num_legal_moves++] = pseudo_moves[i];
-		}
+	if (depth <= 0) {
+		return evaluate(pos);
 	}
 
-	srand(time(NULL));
+	std::array<uint32_t, max_moves> moves;
+	int num_moves = 0;
+	int num_legal_moves = 0;
+	gen_pseudo_moves(pos, moves, num_moves, false);
+	for (int i = 0; i < num_moves; i++) {
+		int32_t move = moves[i];
+		if (make_move(pos, move)) {
+			num_legal_moves++;
+			int32_t score = -negamax(pos, best_move_root, -beta, -alpha, depth - 1, ply + 1);
+			undo_move(pos, move);
 
-	searching = false;
-	std::cout << "bestmove " << get_move_str(legal_moves[rand() % num_legal_moves]) << std::endl;
+			if (score > best_score) {
+				best_score = score;
+				if (ply == 0) {
+					best_move_root = move;
+				}
+			}
+			if (score >= beta) {
+				break;
+			}
+		}
+	}
+	if (num_legal_moves == 0) {
+		int king_sq = get_lsb(pos.pce_bitboards[static_cast<uint32_t>(build_pce(PieceType::KING, pos.side_to_move))]);
+		if (sq_attacked(pos, king_sq, flip_col(pos.side_to_move))) {
+			return -mate_score + ply;
+		}
+		else {
+			return 0;
+		}
+	}
+	return best_score;
 }

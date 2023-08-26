81e45f2a787db3b611d9d0f2ecdc10498e35d182
Daniel <daniel.alp.email@gmail.com>
2023-08-26 02:20:02 -0400

    Implemented temporary method to test if PSQT works. PSQT appears to be implemented correctly.

--- src/search.cpp
+++ src/search.cpp
@@ -1,1 +1,40 @@
+#include "board.h"
+#include "makemove.h"
+#include "movegen.h"
 #include "search.h"
+#include <iostream>
+
+void get_best_move(Position& pos, SearchData& search_data) {
+	uint32_t best_move_root = null_move;
+
+	test_search(pos, search_data, 4);
+}
+
+//Temporary search method used to test PSQTs
+void test_search(Position& pos, SearchData& search_data, int depth) {
+	search_data.nodes++;
+	if (depth == 0) {
+		if (search_data.nodes % 1024 == 0) {
+			print_board(pos);
+			std::cout << "MIDGAME MTRL " << pos.material_midgame_val << std::endl;
+			std::cout << "ENDGAME MTRL " << pos.material_endgame_val << std::endl;
+			std::cout << "MIDGAME PSQT " << pos.psqt_midgame_val << std::endl;
+			std::cout << "ENDGAME PSQT " << pos.psqt_endgame_val << std::endl;
+			std::cout << "PHASE    VAL " << pos.phase_val << std::endl;
+			std::cout << std::endl;
+		}
+		return;
+	}
+
+	std::array<uint32_t, max_moves> moves;
+	int num_moves;
+	gen_pseudo_moves(pos, moves, num_moves, false);
+
+	for (int i = 0; i < num_moves; i++) {
+		uint32_t move = moves[i];
+		if (make_move(pos, move)) {
+			test_search(pos, search_data, depth - 1);
+			undo_move(pos, move);
+		}
+	}
+}
--- src/search.h
+++ src/search.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "board.h"
+#include "movegen.h"
+#include "transposition.h"
+
+constexpr int32_t mate_score = 30000;
+
+struct SearchData {
+	bool searching;
+	uint64_t start_time;
+	uint64_t time_allotted;
+	uint64_t nodes;
+};
+
+void get_best_move(Position& pos, SearchData& search_data);
+
+void test_search(Position& pos, SearchData& search_data, int depth);
